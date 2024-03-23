c35a4905c7f8ef9b398a4d9068604211e34c85f0
Mahesh Savant <smahesh7741@gmail.com>
2024-03-23 10:40:46 +0530

    this keyword code and how to use this keyword

--- encapsulation.java
+++ encapsulation.java
@@ -1,25 +1,26 @@
 class Human{
     private String name;
     private int age;
-
-    public void setAge(int a) {
-        age=a;
+    public String getName() {
+        return name;
     }
-    public void setname(String n) {
-        name=n;
+    public void setName(String name) {
+        this.name = name;
     }
     public int getAge() {
         return age;
     }
-    public String getName() {
-        return name;
+    public void setAge(int age) {
+        this.age = age;
     }
+
+    
 }
 
 public class encapsulation {
     public static void main(String arg[]){
         Human h=new Human();
-        h.setname("Mahesh");
+        h.setName("Mahesh");
         h.setAge(20);
         System.out.println(h.getName()+" "+h.getAge()+" ");
     }
--- forEachLoop.java
+++ forEachLoop.java
@@ -1,4 +1,3 @@
-
 class student{
     int id;
     String name;
