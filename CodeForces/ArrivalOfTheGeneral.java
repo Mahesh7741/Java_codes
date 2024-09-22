afdc6a262e4f5b45cf8e79493416aedf71b649bc
Mahesh Savant <SMAHESH7741@GMAIL.COM>
2024-09-22 17:08:41 +0530

    Multithreading problems

---
+++ CodeForces/ArrivalOfTheGeneral.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ArrivalOfTheGeneral{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] soldiersHeight=new int[n];
+		for(int i=0;i<n;i++){
+			soldiersHeight[i]=sc.next();
+		}
+		
+	}
+}
