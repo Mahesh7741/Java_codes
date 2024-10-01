c6e7dda76c6b65bc8ee6850df21d07d8daf2bb96
Mahesh Savant <SMAHESH7741@GMAIL.COM>
2024-10-02 00:57:45 +0530

    Insertion Sort

--- DSA/Sorting Algorithm/InsertionSort.java
+++ DSA/Sorting Algorithm/InsertionSort.java
@@ -1,9 +1,22 @@
-import java.util*;
+import java.util.*;
 public class InsertionSort{
-	public static void main(String[] args){
-		public static void insertionSort(int[] arr,int n){
-			
+	public static void insertionSort(int[] arr,int n){
+		for(int i=1;i<n;i++){
+				int key=arr[i];
+				int j=i-1;
+				while(j>=0&&arr[j]>key){
+					arr[j+1]=arr[j];
+					j--;
+				}
+				arr[j+1]=key;
+			}	
+	}
+	public static void  print(int[] arr,int size){
+		for(int i=0;i<size;i++){
+			System.out.print(arr[i]+" ");
 		}
+	}
+	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
 		int size=sc.nextInt();
 		int[] arr=new int[size];
