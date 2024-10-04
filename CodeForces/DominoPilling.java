bcf45d12268eb7b6d4b10303f97f38a57d77073c
Mahesh Savant <SMAHESH7741@GMAIL.COM>
2024-10-04 22:32:10 +0530

    Domino piling

--- CodeForces/DominoPilling.java
+++ CodeForces/DominoPilling.java
@@ -1,9 +1,14 @@
 import java.util.*;
-public class DominoPailling{
+public class DominoPilling{
+	public static int dominoPailling(int n,int m){
+		return (n*m)/2;
+	}
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int m=sc.nextInt();
-		
+		int result=dominoPailling(n,m);
+		System.out.println(result);
+		sc.close();
 	}
 }
