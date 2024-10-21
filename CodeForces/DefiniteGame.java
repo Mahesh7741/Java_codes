3afd53b09e191a06db82865ac23de11eb0bfc85f
Mahesh Savant <SMAHESH7741@GMAIL.COM>
2024-10-21 21:59:18 +0530

    Definite Game

---
+++ CodeForces/DefiniteGame.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class DefiniteGame{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		long v=sc.nextLong();
+		if(v==2){
+			System.out.println("2");
+		}else{
+			System.out.println("1");
+		}
+		sc.close();
+	}
+}
