0539bb46d0dbb4798f7f91a754238f29a7074710
Mahesh Savant <SMAHESH7741@GMAIL.COM>
2024-11-24 21:25:56 +0530

    Ternary XOR sum changes

--- CodeForces/TernaryXOR.java
+++ CodeForces/TernaryXOR.java
@@ -3,40 +3,41 @@ import java.util.Scanner;
 public class TernaryXOR {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
 
-        int tc = sc.nextInt();
-
-        while (tc-- > 0) {
+        while (t-- > 0) {
             int n = sc.nextInt();
-            String str = sc.next();
+            String x = sc.next();
 
             StringBuilder a = new StringBuilder();
             StringBuilder b = new StringBuilder();
 
-            for (int i = 0; i < n; i++) {
-                char ch = str.charAt(i);
+            boolean split = false;
 
-                if (ch == '0') {
-                    a.append('0');
-                    b.append('0');
-                } else if (ch == '1') {
-                    if (a.toString().compareTo(b.toString()) >= 0) {
+            for (int i = 0; i < n; i++) {
+                char c = x.charAt(i);
+                if (!split) {
+                    if (c == '0') {
                         a.append('0');
-                        b.append('1');
-                    } else {
+                        b.append('0');
+                    } else if (c == '1') {
                         a.append('1');
                         b.append('0');
+                        split = true;
+                    } else { // c == '2'
+                        a.append('1');
+                        b.append('1');
                     }
-                } else if (ch == '2') {
-                    if (a.toString().compareTo(b.toString()) > 0) {
+                } else {
+                    if (c == '0') {
+                        a.append('0');
+                        b.append('0');
+                    } else if (c == '1') {
+                        a.append('0');
+                        b.append('1');
+                    } else { // c == '2'
                         a.append('0');
                         b.append('2');
-                    } else if (a.toString().compareTo(b.toString()) == 0) {
-                        a.append('1');
-                        b.append('1');
-                    } else {
-                        a.append('2');
-                        b.append('0');
                     }
                 }
             }
