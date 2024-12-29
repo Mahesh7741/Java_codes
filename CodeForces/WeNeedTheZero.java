3e9347885196fd5b1b2ab33561b178a8745017a3
Mahesh Savant <SMAHESH7741@GMAIL.COM>
2024-12-29 17:02:49 +0530

    We Need the Zero

---
+++ CodeForces/WeNeedTheZero.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class WeNeedTheZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long T = sc.nextLong();
+        while (T-- > 0) {
+            long n = sc.nextLong();
+            long xOR = 0;
+            for (int i = 0; i < n; i++) {
+                long a = sc.nextLong();
+                xOR = xOR ^ a;
+            }
+
+            if (xOR == 0) {
+                System.out.println(0);
+            } else if (n % 2 == 1) {
+                System.out.println(xOR);
+            } else {
+                System.out.println(-1);
+            }
+        }
+        sc.close();
+    }
+}
