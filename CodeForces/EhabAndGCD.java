58d74ed6eb075fdce9a2fc0dd765ba8651607bfc
Mahesh Savant <SMAHESH7741@GMAIL.COM>
2025-01-02 11:15:37 +0530

    EhAb AnD gCd

---
+++ CodeForces/EhabAndGCD.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class EhabAndGCD {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            long x = sc.nextLong();
+            System.out.println("1 " + (x - 1));
+        }
+    }
+}
