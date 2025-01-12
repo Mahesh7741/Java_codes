03ddf8f67b1d007255963fab0bbda6e1b3cdd2eb
Mahesh Savant <SMAHESH7741@GMAIL.COM>
2025-01-12 23:23:24 +0530

    1837A - Grasshopper on a Line

---
+++ CodeForces/GrasshopperOnALine.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class GrasshopperOnALine {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long T = sc.nextLong();
+
+        while (T-- > 0) {
+            solve(sc);
+        }
+    }
+
+    public static void solve(Scanner sc) {
+        long N = sc.nextLong();
+        long K = sc.nextLong();
+
+        if (N % K != 0) { 
+            System.out.println(1);
+            System.out.println(N);
+        } else {  
+            System.out.println(2);
+            System.out.println(N - 1 + " " + 1);
+        }
+    }
+}
