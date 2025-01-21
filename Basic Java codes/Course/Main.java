377d5daa7914d8c6ebc2b7eb6e692d232bc3230a
Mahesh Savant <SMAHESH7741@GMAIL.COM>
2025-01-22 00:49:53 +0530

    Abstract class and methode

--- Basic Java codes/Course/Main.java
+++ Basic Java codes/Course/Main.java
@@ -1,5 +1,42 @@
+
+abstract class Animal{
+    private String name="shubham";
+    Animal(){
+        System.out.println("Animal constructor");
+    }
+
+    abstract public void sound();
+    public void sleep(){
+        System.out.println("SLEEP ...");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Dog extends Animal{
+    public void Animal(){
+        System.out.println("override Animal constructor into dog class..." +getName());
+    }
+    public void sleep(){
+        System.out.println("Dog is sleeping ...");
+    }
+    public void sound(){
+        System.out.println("buuu..");
+    }
+}
+
 public class Main{
     public static void main(String[] args){
         
+        Dog obj=new Dog();
+        obj.Animal();
+        obj.sleep();
+        obj.sound();
     }
 }
