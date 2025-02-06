1bc1e29c52709f434aac5cfad9218ca610dbbdc3
Mahesh Savant <SMAHESH7741@GMAIL.COM>
2025-02-06 23:44:25 +0530

    Bay boy

---
+++ CodeForces/BadBoy.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class BadBoy{
+    static Scanner sc=new Scanner(System.in);
+    public static void solve(){
+        int n=sc.nextInt(), m=sc.nextInt(), i=sc.nextInt(), j=sc.nextInt();
+        System.out.println(1 + " " + 1 + " " + n + " " + m);
+    }
+    public static void main(String[] args){
+        int TCS=sc.nextInt();
+        while(TCS-- > 0){
+            solve();
+        }
+    }
+}
